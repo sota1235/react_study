2f838c92c97ceac9d7f0111b01ccd3a8f4c073a2
sota1235 <sota1235@gmail.com>
2015-10-25 18:19:17 +0900

    modify: settings for build js with babel and browserify by gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Gulp plugins
+var gulp       = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+// others
+var browserify = require('browserify');
+var babelify   = require('babelify');
+var source     = require('vinyl-source-stream');
+var buffer     = require('vinyl-buffer');
+var glob       = require('glob');
+
+gulp.task('script', () => {
+  let scripts = glob.sync('./assets/javascripts/*.js');
+  browserify({
+    entries: scripts,
+    debug: true
+  })
+  .transform(babelify)
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(buffer())
+  .pipe(sourcemaps.init({
+    loadMaps: true
+  }))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('./public/javascripts'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('./assets/javascripts/**/*.js', ['script']);
+});
+
+gulp.task('default', ['script']);
