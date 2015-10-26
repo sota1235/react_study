43ddaec697563142fd2e935d0997847323413d23
sota1235 <sota1235@gmail.com>
2015-10-27 00:34:22 +0900

    modify: composing components

--- assets/js/main.babel.js
+++ assets/js/main.babel.js
@@ -8,11 +8,33 @@
 var React    = require('react');
 var ReactDOM = require('react-dom');
 
+var CommentList = React.createClass({
+  render: () => {
+    return (
+      <div className="commentList">
+        Hello, world! I am a comment list.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: () => {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a comment form.
+      </div>
+    );
+  }
+});
+
 var CommentBox = React.createClass({
   render: () => {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
